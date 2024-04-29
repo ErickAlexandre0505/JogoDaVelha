ddcd329b76bd230503938e6cfa39a6693c192bbf
Erick Alexandre <erickalexandre0505@gmail.com>
2024-04-29 16:57:18 -0300

    Implementado função de eempate

--- script.js
+++ script.js
@@ -31,3 +31,42 @@ function render(){
     });
 }
 
+function checkWinner(){
+    for (let i = 0; i < 3; i++) {
+        if (gameBoard[i][0] !== '' && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2]) {
+            return true;
+        }
+        if (gameBoard[0][i] !== '' && gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]) {
+            return true;
+        }
+    }
+    if (gameBoard[0][0] !== '' && gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2]) {
+        return true;
+    }
+    if (gameBoard[0][2] !== '' && gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]) {
+        return true;
+    }
+    return false;
+
+}
+
+function checkDraw(){
+    for(let row of gameBoard){
+        for(let cell of row){
+            if(cell === ""){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function resetGame(){
+    currentPlayer = "X";
+    gameBoard = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    render();
+}
