e484489e0431bdb0e125f56208f034d556467d17
Erick Alexandre <erickalexandre0505@gmail.com>
2024-04-29 16:28:49 -0300

    Implementado função render

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let jogador1 = "X";
-let jogador2 = "O";
+let currentPlayer = "X";
 
-let jogo = {
+let gameBoard = [
     ['', '', ''],
     ['', '', ''],
     ['', '', '']
-};
+];
 
 function handleClick(row,cell){
-    if(jogo[row][cell] === ''){
-        jogo[row][cell] = jogador1;
+    if(gameBoard[row][cell] === ''){
+        gameBoard[row][cell] = currentPlayer;
         render();
+        if(checkWinner()){
+            alert("${currentPlayer} VENCEU ! ");
+            resetGame();
+        }else if(checkDraw()){
+            alert("Empate!");
+            resetGame();
+        }else{
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
     }
 }
+
+function render(){
+    const cells = document.querySelectorAll(".cell");
+    gameBoard.forEach((row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+            cells[rowIndex * 3 + colIndex].innerText = cell;
+        });
+    });
+}
+
